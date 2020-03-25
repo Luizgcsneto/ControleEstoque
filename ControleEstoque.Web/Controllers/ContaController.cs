7f3ea3f950855ebca078b30c327ea3fe3abce87b
Luiz Gonzaga <luizgonzaga15@gmail.com>
2020-03-25 13:35:42 -0300

    Criação do primeiro Model de Login.

--- ControleEstoque.Web/Controllers/ContaController.cs
+++ ControleEstoque.Web/Controllers/ContaController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace ControleEstoque.Web.Controllers
 {
@@ -22,6 +23,25 @@ namespace ControleEstoque.Web.Controllers
             if (!ModelState.IsValid)
             {
                 return View(login);
+            }
+
+            var achou = (login.Usuario == "luizneto" && login.Senha == "2302");
+
+            if (achou)
+            {
+                FormsAuthentication.SetAuthCookie(login.Usuario, login.LembrarMe);
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    RedirectToAction("index","home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("","Login inválido");
             }
 
             return View(login);
--- ControleEstoque.Web/Models/LoginViewModel.cs
+++ ControleEstoque.Web/Models/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace ControleEstoque.Web.Models
 {
     public class LoginViewModel
     {
+        [Display(Name="Usuário:")]
         public String Usuario { get; set; }
+        [Display(Name = "Senha:")]
         public String Senha { get; set; }
+        [Display(Name = "Lembrar Me:")]
         public bool LembrarMe { get; set; }
     }
 }

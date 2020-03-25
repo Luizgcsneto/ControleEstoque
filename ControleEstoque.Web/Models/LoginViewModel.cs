5fe18cdcc3854911192b60dc6bf1d0a555e02d79
Luiz Gonzaga <luizgonzaga15@gmail.com>
2020-03-25 14:00:21 -0300

    Primeira parte da validação funcionando perfeitamente.

--- ControleEstoque.Web/Models/LoginViewModel.cs
+++ ControleEstoque.Web/Models/LoginViewModel.cs
@@ -8,9 +8,14 @@ namespace ControleEstoque.Web.Models
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Informe o Usuário")]
         [Display(Name="Usuário:")]
         public String Usuario { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha")]
+        [DataType(DataType.Password)]
         [Display(Name = "Senha:")]
+
         public String Senha { get; set; }
         [Display(Name = "Lembrar Me:")]
         public bool LembrarMe { get; set; }

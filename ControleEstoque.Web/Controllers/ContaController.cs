b6cad508eb5c57d4259ed8dffc1fc223330c0030
Luiz Gonzaga <luizgonzaga15@gmail.com>
2020-03-25 02:01:30 -0300

    Criando o primeiro form de login e configurando a autenticação e autorzação no Web.confi

---
+++ ControleEstoque.Web/Controllers/ContaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleEstoque.Web.Controllers
+{
+    public class ContaController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+    }
+}

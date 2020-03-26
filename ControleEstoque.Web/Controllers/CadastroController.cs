471c556ad100aeb589d35c3747ba0fcaf7fd848a
Luiz Gonzaga <luizgonzaga15@gmail.com>
2020-03-26 11:43:40 -0300

    Listando corretamente com os dados preenchidos no controller para testar.

--- ControleEstoque.Web/Controllers/CadastroController.cs
+++ ControleEstoque.Web/Controllers/CadastroController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ControleEstoque.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,60 +9,68 @@ namespace ControleEstoque.Web.Controllers
 {
     public class CadastroController : Controller
     {
-        [Authorize]
-        public ActionResult GrupoProduto()
+        private static List<GrupoProdutoModel> _listaGrupoProduto = new List<GrupoProdutoModel>()
         {
-            return View();
-        }
-        [Authorize]
-        public ActionResult MarcaProduto()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult LocalProduto()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult UnidadeMedida()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult Produto()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult Pais()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult Estado()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult Cidade()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult Fornecedor()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult PerfilUsuario()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult Usuario()
-        {
-            return View();
-        }
+            new GrupoProdutoModel() {Id=1, Nome="Livros", Ativo=true },
+            new GrupoProdutoModel() {Id=2, Nome="Cadernos", Ativo=true },
+            new GrupoProdutoModel() {Id=3, Nome="Canetas", Ativo=false },
+            new GrupoProdutoModel() {Id=4, Nome="Lápis", Ativo=false }
+        };
+
+    [Authorize]
+    public ActionResult GrupoProduto()
+    {
+        return View(_listaGrupoProduto);
     }
+    [Authorize]
+    public ActionResult MarcaProduto()
+    {
+        return View();
+    }
+    [Authorize]
+    public ActionResult LocalProduto()
+    {
+        return View();
+    }
+    [Authorize]
+    public ActionResult UnidadeMedida()
+    {
+        return View();
+    }
+    [Authorize]
+    public ActionResult Produto()
+    {
+        return View();
+    }
+    [Authorize]
+    public ActionResult Pais()
+    {
+        return View();
+    }
+    [Authorize]
+    public ActionResult Estado()
+    {
+        return View();
+    }
+    [Authorize]
+    public ActionResult Cidade()
+    {
+        return View();
+    }
+    [Authorize]
+    public ActionResult Fornecedor()
+    {
+        return View();
+    }
+    [Authorize]
+    public ActionResult PerfilUsuario()
+    {
+        return View();
+    }
+    [Authorize]
+    public ActionResult Usuario()
+    {
+        return View();
+    }
+}
 }
